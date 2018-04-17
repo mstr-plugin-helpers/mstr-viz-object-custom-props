4276322f43ea76d9ede3f74d971cad520f9c5493
paulbailey <bailey@dreamshake.net>
2018-04-17 11:40:14 +0100

    Basic tests implemented

---
+++ test/test-custom-props.js
@@ -0,0 +1,35 @@
+import sinon from 'sinon'
+import chai from 'chai'
+import 'chai/register-should'
+import { objectPropertiesMixin } from '../src/obj-custom-props'
+
+chai.use(require('chai-sinon'))
+
+describe('#setObjectCustomProperty()', function () {
+  let spySet = sinon.spy()
+  let vizMock = {
+    setProperty: spySet,
+    setObjectCustomProperty: objectPropertiesMixin.setObjectCustomProperty
+  }
+
+  it('should call setProperty appropriately on the viz', function () {
+    vizMock.setObjectCustomProperty('id', 'prop', 'value')
+    spySet.should.have.been.calledOnce
+    spySet.should.have.been.calledWith('object-id-prop', 'value')
+  })
+})
+
+describe('#getObjectCustomProperty()', function () {
+  let spyGet = sinon.spy()
+  let vizMock = {
+    getProperty: spyGet,
+    getObjectCustomProperty: objectPropertiesMixin.getObjectCustomProperty
+  }
+
+  it('should call getProperty appropriately on the viz', function () {
+    vizMock.getObjectCustomProperty('id', 'prop')
+    spyGet.should.have.been.calledOnce
+    spyGet.should.have.been.calledWith('object-id-prop')
+  })
+
+})
