167cd1307b20cc435e8784d85a8eca7191f4489d
paulbailey <bailey@dreamshake.net>
2018-04-17 14:30:34 +0100

    Test getProperties

--- test/test-custom-props.js
+++ test/test-custom-props.js
@@ -2,6 +2,7 @@ import sinon from 'sinon'
 import chai from 'chai'
 import 'chai/register-should'
 import { objectPropertiesMixin } from '../src/obj-custom-props'
+import { expect } from 'chai'
 
 chai.use(require('chai-sinon'))
 
@@ -49,5 +50,24 @@ describe('#getObjectProperties()', function () {
     sinon.spy(vizMock, 'getProperties')
     vizMock.getObjectProperties()
     vizMock.getProperties.should.have.been.calledOnce
+  })
+
+  it('should return custom properties with an appropriately formatted key', function () {
+    let spyGet = sinon.spy()
+    let vizMock = {
+      getProperties: function () { return {
+        'object-id-key': 'value',
+        'object-id-key2': 'value2',
+        'hello-dave': 'ignore'
+      } },
+      getObjectProperties: objectPropertiesMixin.getObjectProperties
+    }
+    sinon.spy(vizMock, 'getProperties')
+    let results = vizMock.getObjectProperties('id')
+    vizMock.getProperties.should.have.been.calledOnce
+    Object.keys(results).length.should.equal(2)
+    results.key.should.equal('value')
+    results.key2.should.equal('value2')
+    expect(results['hello-dave']).to.equal(undefined)
   })
 })
