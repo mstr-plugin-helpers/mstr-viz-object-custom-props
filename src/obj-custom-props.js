15f3e477ea97609282dbf92c984f4dab792b3a14
Paul Bailey <bailey@dreamshake.net>
2018-05-02 14:35:13 +0100

    New storage format, with metadata

--- src/obj-custom-props.js
+++ src/obj-custom-props.js
@@ -1,26 +1,108 @@
+import uuidv4 from 'uuid/v4'
+
+/**
+ * Adds the mixin to the target object
+ * 
+ * @param {object} target 
+ */
+function addMixin(target) {
+  Object.keys(objectPropertiesMixin).forEach((key) => {
+    target[key] = objectPropertiesMixin[key]
+  })
+}
+/**
+ * Returns a key for an object that can be used as a custom property in MicroStrategy
+ * 
+ * @param {string} objectId 
+ * @returns {string} MicroStrategy-safe custom property key
+ */
+function getObjectCustomPropertyKey (objectId) {
+  return `obj-${objectId}`
+}
+
+function updateMetadataUUID (viz) {
+  let metadata = viz.getProperty('mstr-viz-object-custom-props')
+  metadata.uuid = uuidv4()
+  viz.setProperty('mstr-viz-object-custom-props', metadata, { suppressData: true })
+}
+
+function checkMetdataVersion (viz) {
+  let metadata = viz.getProperty('mstr-viz-object-custom-props')
+  if (!metadata) {
+    upgradeCustomObjectProperties (viz)
+    metadata = { 
+      version: 'v1',
+      uuid: uuidv4()
+    }
+    viz.setProperty('mstr-viz-object-custom-props', metadata)
+  }
+}
+
+function upgradeCustomObjectProperties (viz) {
+  let properties = viz.getProperties()
+  let objCustProps = {}
+  Object.keys(properties).forEach((key) => {
+    var parts = key.split('-')
+    if (parts.length == 3) {
+      let prefix = parts[0]
+      let objID = parts[1]
+      let propName = parts[2]
+      if (prefix === 'metric' || prefix === 'object') {
+        let prefixedObjID = 'obj-' + objID
+        if (!objCustProps[prefixedObjID]) {
+          objCustProps[prefixedObjID] = {}
+        }
+        objCustProps[prefixedObjID][propName] = properties[key]
+        delete properties[key]
+      }
+    }
+  })
+  Object.keys(objCustProps).forEach((key) => {
+    viz.setProperty(key, objCustProps[key], { suppressData: true })
+  })
+}
+
+function deleteProperty (prop) {
+  let properties = this.getProperties()
+  delete properties[prop]
+  updateMetadataUUID(this)
+}
+
 /**
  * Saves a property pertaining to a specific object into the MicroStrategy object model
  *
- * @param {any} objectId the object UUID
- * @param {any} prop the property name to save
- * @param {any} value the property value to save
- * @returns {undefined}
+ * @param {string} objectId the object UUID
+ * @param {string} prop the property name to save
+ * @param {string} value the property value to save
+ * @returns {void}
  */
 function setObjectCustomProperty (objectId, prop, value, configObject) {
-  let objectKey = 'object-' + objectId + '-' + prop
-  this.setProperty(objectKey, value, configObject)
+  checkMetdataVersion(this)
+  let objectKey = getObjectCustomPropertyKey(objectId)
+  let objectProperties = this.getObjectProperties(objectId)
+  if (!objectProperties) {
+    objectProperties = {}
+  }
+  let oldPropValue = objectProperties[prop]
+  if (value != oldPropValue) {
+      objectProperties[prop] = value
+  }
+  this.setProperty(objectKey, objectProperties, configObject)
 }
+
 /**
  * Retrieves a property pertaining to a specific object from the MicroStrategy object model
  *
- * @param {any} objectId the object UUID
- * @param {any} prop the property name to retrieve
+ * @param {string} objectId the object UUID
+ * @param {string} prop the property name to retrieve
  * @returns {string} the property value
  */
 function getObjectCustomProperty (objectId, prop) {
-  let objectKey = 'object-' + objectId + '-' + prop
-  return this.getProperty(objectKey)
+  checkMetdataVersion(this)
+  let objectProps = this.getObjectProperties(objectId)
+  return objectProps ? objectProps[prop] : undefined
 }
+
 /**
  * Retrieves all properties pertaining to a specific object from the MicroStrategy object model
  *
@@ -28,34 +110,19 @@ function getObjectCustomProperty (objectId, prop) {
  * @returns {Object} a map of all the properties
  */
 function getObjectProperties (objectId) {
+  checkMetdataVersion(this)
   let props = this.getProperties()
-  let objectProps = Object.keys(props)
-    .map(function (key) {
-      if (key.startsWith('object-' + objectId)) {
-        return key
-      }
-    })
-    .filter(function (k) {
-      return k !== undefined
-    })
-
-  let returnObj = {}
-  for (let key in objectProps) {
-    let oldKey = objectProps[key]
-    let value = props[oldKey]
-    let newKey = oldKey.split('-')[2]
-    returnObj[newKey] = value
-  }
-
-  return returnObj
+  return props[getObjectCustomPropertyKey(objectId)]
 }
 
 let objectPropertiesMixin = {
+  deleteProperty,
   getObjectProperties,
   getObjectCustomProperty,
   setObjectCustomProperty
 }
 
 export {
+  addMixin,
   objectPropertiesMixin
 }
--- test/test-custom-props.js
+++ test/test-custom-props.js
@@ -1,70 +1,78 @@
 import sinon from 'sinon'
 import chai from 'chai'
 import 'chai/register-should'
-import { objectPropertiesMixin } from '../src/obj-custom-props'
+import { objectPropertiesMixin, addMixin } from '../src/obj-custom-props'
 import { expect } from 'chai'
 
 chai.use(require('chai-sinon'))
 
-describe('#setObjectCustomProperty()', function () {
-  let spySet = sinon.spy()
-  let vizMock = {
-    setProperty: spySet,
-    setObjectCustomProperty: objectPropertiesMixin.setObjectCustomProperty
+class VizMock {
+  constructor(properties) {
+    if (properties) {
+      this.properties = properties
+    } else {
+      this.properties = {}
+    }
   }
+  getProperties() {
+    return this.properties
+  }
+
+  getProperty(prop) {
+    return this.properties[prop]
+  }
+  setProperty(prop, value) {
+    this.properties[prop] = value
+  }
+}
+
+describe('#setObjectCustomProperty()', function () {
+  let vizMock = new VizMock()
+  let spySet = sinon.spy(vizMock, 'setProperty')
+  addMixin(vizMock)
 
   it('should call setProperty appropriately on the viz', function () {
     vizMock.setObjectCustomProperty('id', 'prop', 'value')
-    spySet.should.have.been.calledOnce
-    spySet.should.have.been.calledWith('object-id-prop', 'value')
+    spySet.should.have.been.calledTwice
+    spySet.should.have.been.calledWith('obj-id', { prop: 'value' })
   })
 })
 
 describe('#getObjectCustomProperty()', function () {
-  let spyGet = sinon.spy()
-  let vizMock = {
-    getProperty: spyGet,
-    getObjectCustomProperty: objectPropertiesMixin.getObjectCustomProperty
-  }
+  let vizMock = new VizMock()
+  addMixin(vizMock)
 
   it('should call getProperty appropriately on the viz', function () {
+    let spyGetProps = sinon.spy(vizMock, 'getObjectProperties')
     vizMock.getObjectCustomProperty('id', 'prop')
-    spyGet.should.have.been.calledOnce
-    spyGet.should.have.been.calledWith('object-id-prop')
+    spyGetProps.should.have.been.calledOnce
   })
 })
 
 describe('#getObjectProperties()', function () {
-  let spyGet = sinon.spy()
-  let vizMock = {
-    getProperty: spyGet,
-    getObjectCustomProperty: objectPropertiesMixin.getObjectCustomProperty
-  }
-
   it('should call getProperties appropriately', function () {
     let spyGet = sinon.spy()
-    let vizMock = {
-      getProperties: function () { return {} },
-      getObjectProperties: objectPropertiesMixin.getObjectProperties
-    }
+    let vizMock = new VizMock()
+    addMixin(vizMock)
     sinon.spy(vizMock, 'getProperties')
     vizMock.getObjectProperties()
-    vizMock.getProperties.should.have.been.calledOnce
+    vizMock.getProperties.should.have.been.calledTwice
   })
 
   it('should return custom properties with an appropriately formatted key', function () {
-    let spyGet = sinon.spy()
-    let vizMock = {
-      getProperties: function () { return {
-        'object-id-key': 'value',
-        'object-id-key2': 'value2',
-        'hello-dave': 'ignore'
-      } },
-      getObjectProperties: objectPropertiesMixin.getObjectProperties
-    }
+    let vizMock = new VizMock({
+      'obj-id': {
+        key: 'value',
+        key2: 'value2'
+      },
+      'hello-dave': 'ignore'
+    })
+
+    addMixin(vizMock)
+
     sinon.spy(vizMock, 'getProperties')
     let results = vizMock.getObjectProperties('id')
-    vizMock.getProperties.should.have.been.calledOnce
+    vizMock.getProperties.should.have.been.calledTwice
     Object.keys(results).length.should.equal(2)
     results.key.should.equal('value')
     results.key2.should.equal('value2')
